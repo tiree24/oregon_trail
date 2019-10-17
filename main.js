9830dfb78cf999191551792122fca31f4f1b5293
Jackson <tireej@amazon.com>
2019-10-17 16:07:27 -0400

    updates

---
+++ main.js
@@ -0,0 +1,25 @@
+function traveler(){
+    this.name = name;
+    this.food = 1;
+    this.isHealthy = isHealthy;
+    this.hunt = hunt;
+}
+
+traveler.prototype.hunt = function(){
+    this.food++
+    if () {
+      
+    }
+ 
+}
+
+console.log(traveler)
+
+
+
+function wagon(){
+    this.capacity = 5
+    this.passengers = 0
+}
+
+console.log(wagon)
